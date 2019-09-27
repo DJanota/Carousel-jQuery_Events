241d587939570c9c9f73bebe0079c751f3151e2c
Dawid Janota <6dawid.janota@wp.pl>
2019-09-27 21:19:17 +0200

    jQuery/JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-$(function(){
-   var carouselList = $("#carousel ul");
+$(function () {
+    var carouselList = $("#carousel ul");
 
-   function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-}
-   
-   function changeSlide() { 
-      carouselList.animate({'marginLeft':-400}, 400, moveFirstSlide);
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({
+            marginLeft: 0
+        });
     }
-  
-  
-  setInterval(changeSlide, 3000);
+
+    function changeSlide() {
+        carouselList.animate({
+            'marginLeft': -500
+        }, 500, moveFirstSlide);
+    }
+
+
+    setInterval(changeSlide, 3000);
 
 });
